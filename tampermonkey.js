0831a90c02dd53fb2a42240f81379d8b23763f47
bbbyqq <2287532812@qq.com>
2022-08-01 15:28:24 +0800

    1.更改画中画位置 2.新增判断当前窗口是否处于画中画模式，解决有时需要点击两次按钮才能进入画中画模式的bug

--- tampermonkey.js
+++ tampermonkey.js
@@ -1,31 +1,39 @@
 // ==UserScript==
 // @name         115画中画
 // @namespace    http://tampermonkey.net/
-// @version      1.0
+// @version      1.1
 // @description  在115播放界面加入画中画按钮!
 // @author       bbbyqq
 // @match        *://v.anxia.com/*
-// @grant        none
 // @license      bbbyqq
 // ==/UserScript==
 
 (function () {
   'use strict'
-  const headline = document.querySelector('.vt-headline')
-  const btn = document.createElement('a') // 创建一个按钮
-  btn.textContent = '画中画'
-  btn.className = 'btn-opendir'
-  btn.href = 'javascript:;'
-  headline.appendChild(btn)
+  const menu = document.querySelector('.bar-side ul')
+  const li = document.createElement('li') // 创建一个按钮
+  const a = document.createElement('a') // 创建一个按钮
+  a.href = 'javascript:;'
+  a.textContent = '画中画'
+  a.className = 'btn-cle'
+  li.appendChild(a)
+  menu.insertBefore(li, menu.childNodes[2])
+
+  const videoElement = document.getElementById('js-video')
 
   // 绑定按键点击功能
-  let flag = false
-  btn.onclick = function () {
-    flag = !flag
-    if (flag) {
-      document.getElementById('js-video').requestPictureInPicture()
-    } else {
-      document.exitPictureInPicture()
+  a.onclick = function () {
+    if (
+      document.pictureInPictureEnabled &&
+      !videoElement.disablePictureInPicture) {
+      try {
+        if (document.pictureInPictureElement) {
+          document.exitPictureInPicture()
+        }
+        videoElement.requestPictureInPicture()
+      } catch (err) {
+        console.error(err)
+      }
     }
   }
 })()
